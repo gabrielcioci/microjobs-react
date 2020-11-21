1036ed5f139473b104c82528159111206dacb447
gabrielcioci <ciocigabi@gmail.com>
2020-11-21 13:17:23 +0100

    modals on mobile

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,7 +7,7 @@ const Modal = (props) => {
     const dispatch = useDispatch()
     return (
         <div className="absolute modal-container top-0 left-0 right-0 bottom-0">
-            <div className="modal md:w-1/4 px-6 py-4 rounded shadow-md bg-white">
+            <div className="modal fixed w-full left-0 bottom-0 md:left-auto md:bottom-auto md:relative md:w-1/2 lg:w-1/3 xl:w-1/4 px-6 py-4 rounded shadow-md bg-white">
                 <div className={`modal-header flex items-center ${type && type === 'job-details' ? 'mb-4' : 'mb-6'}`}>
                     <div
                         className={`text-gray-700 text-xl ${type && type === 'job-details' && 'text-indigo-600 text-2xl'}`}>{title}</div>
